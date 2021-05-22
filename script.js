955d41856d01d8ae25b04bcdcd7d7f2fa83db149
Abdulqadir90 <abdiqaadirabdillahi@gmail.com>
2021-05-22 23:49:56 +0100

    changed colours

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const container = document.getElementById('container');
-const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71'];
+const colors = ['#e74c3c', '#8e44ad', '#FF0000', '#e67e22', '#2ecc71'];
 const SQUARES = 700;
 
 for(let i = 0; i < SQUARES; i++) {
